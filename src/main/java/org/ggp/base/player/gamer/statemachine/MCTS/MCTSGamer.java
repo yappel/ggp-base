0837fdbaccb55fc6f986410c8fa834d2765d3318
Remco de Vos <vos.remcode@gmail.com>
2017-03-31 01:07:32 +0200

    Added missing initialisation for the InitialStateMachine

--- src/main/java/org/ggp/base/player/gamer/statemachine/MCTS/MCTSGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/MCTS/MCTSGamer.java
@@ -13,9 +13,11 @@ import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 import java.util.List;
 import java.util.Map;
@@ -41,8 +43,7 @@ public class MCTSGamer extends StateMachineGamer {
 
 	@Override
 	public StateMachine getInitialStateMachine() {
-		// TODO Auto-generated method stub
-		return null;
+		return new CachedStateMachine(new ProverStateMachine());
 	}
 
 	@Override
